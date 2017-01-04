bbf5efb1fdf8065d8762796d71808a2a666e5462
jmtysonjr <jtyson@pobox.com>
2017-01-04 13:40:10 -0500

    cherry-pick attempt #2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
 <<<<<<< HEAD
-    System.out.println("Hello World")
-=======
     System.out.println("Hello World");
 >>>>>>> 871d4a1... Hello World - Java Edition
   }
